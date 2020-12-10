901fbb6d5642efcb1d8e492cf567a3e058db004f
hironaga0213 <72966985+hironaga0213@users.noreply.github.com>
2020-12-10 12:14:15 +0900

    いいねランキング

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,7 @@
 class Topic < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
   has_one_attached :image
 
   with_options presence: true do
